ed99d61451d98f2bb10d277434a5b4fe4e5b39b7
ctoothaker1 <ctoothaker1@gmail.com>
2024-09-17 12:51:25 -0400

    TextView size changes when a number is selected from the spinner

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -16,17 +16,30 @@ class MainActivity : AppCompatActivity() {
 //        val numberArray = arrayOf(int)
 //        for (i in 1..20){
 //            numberArray.add(i*5)
-//
-//        }
-        val numberArray = Array(20) {index -> (index+1)*5}
+//       }
         //println(numberArray)
         //println(numberArray.joinToString(" "))
+        val numberArray = Array(20) { index -> (index + 1) * 5 }
         /* TODO Step 3: Create adapter to display items from array in Spinner */
-        spinner.adapter = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item, numberArray)
 
+        spinner.adapter =
+            ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, numberArray)
 
         // TODO Step 4: Change TextView's text size to the number selected in the Spinner */
-        //spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>, p1: View?, position: Int, id: Long) {
+                p1?.run {
+                    val selectedSize = numberArray[position]
+                    displayTextView.textSize = selectedSize.toFloat()
+                }
 
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // do nothing
+            }
+
+
+        }
     }
 }
